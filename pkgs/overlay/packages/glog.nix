5c867e568353aaac64119da3790075816ae1c33e
kilyanni <github@nkilian.org>
2026-08-04 12:03:07 +0200

    opencv4: init

---
+++ pkgs/overlay/packages/glog.nix
@@ -0,0 +1,15 @@
+# glog for wasix, needed by opencv_contrib's sfm module.
+{
+  prev,
+  helpers,
+  ...
+}:
+helpers.libTweaks {
+  # "Platform not supported by glog": its platform.h whitelist has no wasi entry.
+  # Emscripten is the closest match, also lacking SYS_write and /bin/mail.
+  postPatch = ''
+    substituteInPlace src/glog/platform.h \
+      --replace-fail '#elif defined(__EMSCRIPTEN__)' '#elif defined(__EMSCRIPTEN__) || defined(__wasi__)'
+  '';
+}
+prev.glog
