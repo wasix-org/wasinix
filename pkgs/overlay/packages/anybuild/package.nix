dc959f63173e3c02b2b6a26dd4b26a9020bafcb6
Christoph Herzog <chris@theduke.at>
2026-08-07 13:20:39 +0200

    pkgs: set anybuild guest path

--- pkgs/overlay/packages/anybuild/package.nix
+++ pkgs/overlay/packages/anybuild/package.nix
@@ -7,7 +7,13 @@
 }:
 helpers.libTweaks {
   passthru.wasix.shipped = true;
-  passthru.wasmer.dependencies = [preferredProfilePackages.bash];
+  passthru.wasmer = {
+    dependencies = [preferredProfilePackages.bash];
+    commandEnv = {
+      anybuild.PATH = "/bin:/usr/bin";
+      shipit.PATH = "/bin:/usr/bin";
+    };
+  };
 } (toolchain.anybuild.override {
   inherit (final) rustPlatform stdenv;
   shellPath = "/bin/bash";
--- pkgs/overlay/packages/anybuild/tests/basic.nix
+++ pkgs/overlay/packages/anybuild/tests/basic.nix
@@ -8,4 +8,28 @@
     wasixPkgs = [wasmerPkgs.anybuild];
     script = "anybuild --version";
   };
+
+  local-build = testLib.mkWasixRun {
+    name = "anybuild-local-build";
+    wasixPkgs = [wasmerPkgs.anybuild];
+    script = ''
+      mkdir project
+      printf 'hello\n' > project/index.html
+      cat > project/Anybuild <<'EOF'
+      load("//anybuild/tools:staticfile.bzl", "staticfile_build", "staticfile_config", "staticfile_serve")
+
+      config = staticfile_config(schema = 1, sws_version = "2.38.0")
+      build = staticfile_build(config)
+      staticfile_serve(
+          config,
+          build,
+          name = "smoke",
+          build_post = [run("bash -c 'printf built > bash-ran'")],
+      )
+      EOF
+
+      anybuild build project --skip-prepare
+      test "$(cat project/.anybuild/local/build/opt/static_app/bash-ran)" = built
+    '';
+  };
 }
