618ce57cfd4fe7e2e4192e9375d7c1fa82cefda3
kilyanni <github@nkilian.org>
2026-08-17 06:45:54 +0200

    checks: repair remaining guest suites

--- pkgs/overlay/python-packages/cython.nix
+++ pkgs/overlay/python-packages/cython.nix
@@ -1,9 +1,11 @@
 {
   pyprev,
+  pyfinal,
   helpers,
   ...
 }:
 helpers.libTweaks {
+  passthru.wasixDeclaredCheckInputs = [pyfinal.numpy pyfinal.setuptools];
   # The runner uses a process pool above one worker; WASIX has no process semaphores.
   preCheck = ''
     export NIX_BUILD_CORES=1
