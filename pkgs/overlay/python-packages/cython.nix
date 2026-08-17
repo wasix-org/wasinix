1b64c0fc24c7e361c8a227908baedcaa055d6fed
kilyanni <github@nkilian.org>
2026-08-17 08:07:56 +0200

    checks: extend the Cython compiler test timeout

--- pkgs/overlay/python-packages/cython.nix
+++ pkgs/overlay/python-packages/cython.nix
@@ -6,6 +6,7 @@
 }:
 helpers.libTweaks {
   passthru.wasixDeclaredCheckInputs = [pyfinal.numpy pyfinal.setuptools];
+  passthru.wasix.emulatedCheck.timeout = 3600;
   installCheckPhase = _: ''
     export HOME="$NIX_BUILD_TOP"
     ${pyfinal.python.interpreter} runtests.py -j1 --no-code-style --cython-only --no-refnanny
