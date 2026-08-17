f8b28ea3eef97a93ab4594e31478af037c08f5d4
kilyanni <github@nkilian.org>
2026-08-17 17:35:10 +0200

    checks: trim ContourPy test inputs

--- pkgs/overlay/python-packages/contourpy.nix
+++ pkgs/overlay/python-packages/contourpy.nix
@@ -1,10 +1,17 @@
 {
   pyprev,
+  pyfinal,
   helpers,
   ...
 }:
 helpers.libTweaks {
   # These invoke native development tools from inside the WASIX guest.
   disabledTests = ["test_cppcheck" "test_mypy"];
+  passthru.wasixDeclaredCheckInputs = [
+    pyfinal.matplotlib
+    pyfinal.pillow
+    pyfinal.pytestCheckHook
+    pyfinal.wurlitzer
+  ];
 }
 pyprev.contourpy
