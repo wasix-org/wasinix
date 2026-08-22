931e9982aefcfdef4e5d728c7a3b9163d5df1100
kilyanni <github@nkilian.org>
2026-08-22 22:57:51 +0200

    php: expose extension package sets

---
+++ pkgs/overlays/p/php/extensions.nix
@@ -0,0 +1,46 @@
+{
+  final,
+  lib,
+}: let
+  getDev = lib.getDev;
+in {
+  imagick =
+    final.buildPackages.runCommand "php-extension-imagick-3.8.1" {
+      passthru = {
+        extensionName = "imagick";
+        configureFlag = "--with-imagick";
+        buildInputs = [final.imagemagick];
+        env = {
+          IM_IMAGEMAGICK_CFLAGS = "-I${getDev final.imagemagick}/include/ImageMagick -DIM_MAGICKWAND_HEADER_STYLE_SEVEN -DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_CHANNEL_MASK_DEPTH=32";
+          IM_IMAGEMAGICK_LIBS = "-lMagickWand-7.Q16HDRI -lMagickCore-7.Q16HDRI -ltiff -lz -ldeflate -ljpeg -llzma -lzstd -lpng16 -lwebpmux -lwebpdemux -lwebp -lsharpyuv -lfreetype -lxml2 -lcurl -lssl -lcrypto";
+        };
+      };
+    } ''
+      mkdir -p "$out"
+      tar --strip-components=1 -xf ${final.fetchurl {
+        url = "https://pecl.php.net/get/imagick-3.8.1.tgz";
+        hash = "sha256-OjWHwKUkwX0NrZZzoWC5DNd26DaDhHThc7VJ7YZDUu4=";
+      }} -C "$out"
+      substituteInPlace "$out/config.m4" \
+        --replace-fail \
+          '# This line checks that ImageMagick is available, and
+      # meets our minimum supported version. TODO change to 6.7.0
+      IM_FIND_IMAGEMAGICK([6.5.3], [$PHP_IMAGICK])' \
+          ""
+    '';
+
+  igbinary =
+    final.buildPackages.runCommand "php-extension-igbinary-edda7101" {
+      passthru = {
+        extensionName = "igbinary";
+        configureFlag = "--enable-igbinary";
+      };
+    } ''
+      cp -R --no-preserve=mode,ownership ${final.fetchFromGitHub {
+        owner = "igbinary";
+        repo = "igbinary";
+        rev = "edda7101adf583df047d028a154abf3bf04ced61";
+        hash = "sha256-EY3fSQjR0/tuEyNvY7ZYpArtmQNebbMyoa2OhGVkWvE=";
+      }} "$out"
+    '';
+}
--- pkgs/overlays/p/php/tests/extensions.nix
+++ pkgs/overlays/p/php/tests/extensions.nix
@@ -4,30 +4,48 @@
   testLib,
   ...
 }: let
+  extensionSetNames = ["php"] ++ builtins.attrNames (import ../versions.nix);
+  extensionSetsMatch = builtins.all (name: let
+    alias = crossPkgs.${"${name}Extensions"};
+    extensions = crossPkgs.${name}.extensions;
+  in
+    alias.recurseForDerivations
+    && toString alias.igbinary == toString extensions.igbinary
+    && toString alias.imagick == toString extensions.imagick)
+  extensionSetNames;
   phpIgbinaryOnly = crossPkgs.php85.withExtensions ({all, ...}: [all.igbinary]);
   phpInt64IgbinaryOnly = crossPkgs.php85-int64.withExtensions ({all, ...}: [all.igbinary]);
-in {
-  extensions-default = testLib.mkWasixRun {
-    name = "php85-extensions-default";
-    wasixPkgs = [(makeWasmerPackage {package = crossPkgs.php85;}).shim];
-    script = ''
-      php -r 'exit(extension_loaded("igbinary") && extension_loaded("imagick") ? 0 : 1);'
-    '';
-  };
+in
+  assert extensionSetsMatch; {
+    package-sets = testLib.mkScriptRun {
+      name = "php-extension-package-sets";
+      packages = [];
+      script = ''
+        echo "php extension package sets match"
+      '';
+    };
 
-  igbinary-only = testLib.mkWasixRun {
-    name = "php85-igbinary-only";
-    wasixPkgs = [(makeWasmerPackage {package = phpIgbinaryOnly;}).shim];
-    script = ''
-      php -r 'exit(extension_loaded("igbinary") && !extension_loaded("imagick") ? 0 : 1);'
-    '';
-  };
+    extensions-default = testLib.mkWasixRun {
+      name = "php85-extensions-default";
+      wasixPkgs = [(makeWasmerPackage {package = crossPkgs.php85;}).shim];
+      script = ''
+        php -r 'exit(extension_loaded("igbinary") && extension_loaded("imagick") ? 0 : 1);'
+      '';
+    };
 
-  int64-igbinary-only = testLib.mkWasixRun {
-    name = "php85-int64-igbinary-only";
-    wasixPkgs = [(makeWasmerPackage {package = phpInt64IgbinaryOnly;}).shim];
-    script = ''
-      php -r 'exit(PHP_INT_SIZE === 8 && extension_loaded("igbinary") && !extension_loaded("imagick") ? 0 : 1);'
-    '';
-  };
-}
+    igbinary-only = testLib.mkWasixRun {
+      name = "php85-igbinary-only";
+      wasixPkgs = [(makeWasmerPackage {package = phpIgbinaryOnly;}).shim];
+      script = ''
+        php -r 'exit(extension_loaded("igbinary") && !extension_loaded("imagick") ? 0 : 1);'
+      '';
+    };
+
+    int64-igbinary-only = testLib.mkWasixRun {
+      name = "php85-int64-igbinary-only";
+      wasixPkgs = [(makeWasmerPackage {package = phpInt64IgbinaryOnly;}).shim];
+      script = ''
+        php -r 'exit(PHP_INT_SIZE === 8 && extension_loaded("igbinary") && !extension_loaded("imagick") ? 0 : 1);'
+      '';
+    };
+  }
