b140cba73949eabc2de602221ec01f54479f4dda
kilyanni <github@nkilian.org>
2026-08-26 20:35:27 +0200

    php: follow nixpkgs package definitions

--- pkgs/overlays/p/php/versions.nix
+++ pkgs/overlays/p/php/versions.nix
@@ -1,32 +1,18 @@
 {
-  php74 = {
-    version = "7.4.33";
-    rev = "php-7.4.33";
-    hash = "sha256-+tXDSFZNqL3qRiovjAymujYwV4eIj0PhngdKFQNioXs=";
+  historical = {
+    php74 = {
+      version = "7.4.33";
+      hash = "sha256-+tXDSFZNqL3qRiovjAymujYwV4eIj0PhngdKFQNioXs=";
+    };
+    php81 = {
+      version = "8.1.34";
+      hash = "sha256-03kBZNWu7r3xHjuAWSAgtQxfiYSoyCgDzo2hnpfJJew=";
+    };
   };
-  php81 = {
-    version = "8.1.34";
-    rev = "php-8.1.34";
-    hash = "sha256-03kBZNWu7r3xHjuAWSAgtQxfiYSoyCgDzo2hnpfJJew=";
-  };
-  php82 = {
-    version = "8.2.31";
-    rev = "php-8.2.31";
-    hash = "sha256-8OrNn0SJYhY3q0R2r2PGCemSxIpzevpExKVZlIzU7z8=";
-  };
-  php83 = {
-    version = "8.3.31";
-    rev = "php-8.3.31";
-    hash = "sha256-Cg+jhqTNUBnWFeGugL5YhyOgF9v105GHSsQyNeqHghU=";
-  };
-  php84 = {
-    version = "8.4.22";
-    rev = "php-8.4.22";
-    hash = "sha256-VLoijQm3v8nV0WyJL3YZMnsOhn5jRA1mgaNmvFb+8lA=";
-  };
-  php85 = {
-    version = "8.5.7";
-    rev = "php-8.5.7";
-    hash = "sha256-XGEx2x1mNUy7QZvQhuBPJxeewZISDLXt3MNUQK6FRSA=";
-  };
+  nixpkgs = [
+    "php82"
+    "php83"
+    "php84"
+    "php85"
+  ];
 }
