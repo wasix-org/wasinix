5a52d940e41e07e4e5751778ed207a465f442567
kilyanni <github@nkilian.org>
2026-08-26 10:05:57 +0200

    python: add a pip command

--- pkgs/overlays/p/python3/tests/pip-usable.nix
+++ pkgs/overlays/p/python3/tests/pip-usable.nix
@@ -17,6 +17,9 @@ helpers.forEachPython packages.preferred ({
       python${pyVer} -m pip --version | tee out.log
       grep -q '^pip ' out.log
 
+      pip --version | tee command.log
+      grep -q '^pip ' command.log
+
       cp ${./pip-usable-check.py} check.py
       python${pyVer} check.py | tee install.log
       grep -q PIP_OK install.log
